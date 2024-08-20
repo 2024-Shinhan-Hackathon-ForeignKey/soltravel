e338d384ca0e1fccb1ca42b4fe9063538a5d1517
허동원 <heo_dongwon@naver.com>
2024-08-20 15:17:38 +0900

    [BE]build: Bean 추가 설정 이동 및 시큐리티 설정 변경

--- backend/src/main/java/com/ssafy/soltravel/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/soltravel/config/SecurityConfig.java
@@ -1,87 +1,78 @@
 package com.ssafy.soltravel.config;
 
-import com.ssafy.soltravel.handler.OAuth2SuccessHandler;
-import com.ssafy.soltravel.service.user.PrincipalOauthUserService;
+import com.ssafy.soltravel.service.user.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final PrincipalOauthUserService principalOauthUserService;
-    private final OAuth2UserService oauth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+  private final UserService userService;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(CsrfConfigurer::disable)
-                .httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable())
-                .authorizeHttpRequests(
-                        request -> request.requestMatchers(
-                                "/", "/api/v1/auth/**", "/oauth2/**", "/swagger-ui/**", "/error", "index.html",
-                                "/api/upload", "api/list", "/api/v1/main/**", "/v3/api-docs/**").permitAll()
-                                .anyRequest().authenticated()
-                        ).oauth2Login(
-                                oauth2 -> oauth2
-                                        .authorizationEndpoint(endPoiont -> endPoiont.baseUri("/URL"))
-                                        .redirectionEndpoint(endPoiont -> endPoiont.baseUri("/redirect URL"))
-                                        .userInfoEndpoint(endPoiont -> endPoiont.userService(oauth2UserService))
-                                        .successHandler(oAuth2SuccessHandler)
-                        ).exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(new FailedAuthenticatoinEntryPoint())
-                );//TODO: JWT 필터 추가 필요
+  @Value("${server.front.url}")
+  private String FRONT_URL;
 
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .cors(Customizer.withDefaults())                 //등록된 빈에서 CORS 커스텀 설정 찾아서 등록
+        .csrf(AbstractHttpConfigurer::disable)           //csrf 비활성화
+        .authorizeHttpRequests(requests -> requests      //특정 uri만 허용하고 나머지는 인증받아야함
+            .requestMatchers("/", "/login", "/join").permitAll()
+            .anyRequest().authenticated()
+        ).formLogin(form -> form
+            .loginPage(FRONT_URL + "/login")
+            .defaultSuccessUrl("/", true)
+            .permitAll()
+        ).logout(logout -> logout
+            .permitAll()
+        );
 
-    @Bean
-    protected CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 모든 오리진 허용
-        corsConfiguration.addAllowedMethod("*"); // 모든 메소드 허용
-        corsConfiguration.addAllowedHeader("*"); // 모든 헤더 허용
+    return http.build();
+  }
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration); // 모든 경로에 CORS 설정 적용
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-        return source; // CORS 설정 소스 반환
-    }
-
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring()
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
 }
+
+
 // 인증 실패 시 처리할 엔트리 포인트
 class FailedAuthenticatoinEntryPoint implements AuthenticationEntryPoint {
 
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
-        response.setContentType("application/json"); // 응답 콘텐츠 타입을 JSON으로 설정
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 응답 상태 코드를 401으로 설정
-        response.getWriter()
-                .write("{\"code\" : \"UA\", \"message\" : \"UnAuthorized.\"}"); // 응답 바디에 JSON 형태로 메시지 작성
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+    response.setContentType("application/json"); // 응답 콘텐츠 타입을 JSON으로 설정
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 응답 상태 코드를 401으로 설정
+    response.getWriter()
+        .write("{\"code\" : \"UA\", \"message\" : \"UnAuthorized.\"}"); // 응답 바디에 JSON 형태로 메시지 작성
 
-    }
+  }
 }
