4cc2f2246ace48d8b96785865d8a941cd6e09191
허동원 <heo_dongwon@naver.com>
2024-08-21 00:29:34 +0900

    [BE] Build: import @EnableJpaAuditing

--- backend/src/main/java/com/ssafy/soltravel/BackendApplication.java
+++ backend/src/main/java/com/ssafy/soltravel/BackendApplication.java
@@ -2,6 +2,7 @@ package com.ssafy.soltravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
