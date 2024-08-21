31ac42e2cd58c958f12d36b17fd414e910ace13f
허동원 <heo_dongwon@naver.com>
2024-08-21 22:38:13 +0900

    [BE] Feat: 유저엔티티 수정  - userKey 추가(외부API에 사용)

--- backend/src/main/java/com/ssafy/soltravel/domain/User.java
+++ backend/src/main/java/com/ssafy/soltravel/domain/User.java
@@ -1,62 +1,68 @@
 package com.ssafy.soltravel.domain;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long id;
 
-    @Column
-    private String name;
+  @Column
+  private String name;
 
-    @Column
-    private String password;
+  @Column
+  private String password;
 
-    @Column
-    private String email;
+  @Column
+  private String email;
 
-    @Column
-    private String phone;
+  @Column
+  private String phone;
 
-    @Column
-    private String address;
+  @Column
+  private String address;
 
-    @Column(name = "register_at")
-    private LocalDateTime registerAt;
+  @Column(name = "register_at")
+  private LocalDate registerAt;
 
-    @Column(name = "is_exit")
-    private Boolean isExit;
+  @Column(name = "is_exit")
+  private Boolean isExit;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Column(name = "user_key")
+  private String userKey;
 
 
-    /*
-    * 생성 메서드
-    */
-    public static User createUser(String name, String password, String email, String phone, String address) {
-        User user = new User();
-        user.name = name;
-        user.password = password;
-        user.email = email;
-        user.phone = phone;
-        user.address = address;
-        user.registerAt = LocalDateTime.now();
-        user.role = Role.USER;
-        return user;
-    }
+  /*
+   * 생성 메서드
+   */
+  public static User createUser(String name, String password, String email, String phone,
+      String address) {
+    User user = new User();
+    user.name = name;
+    user.password = password;
+    user.email = email;
+    user.phone = phone;
+    user.address = address;
+    user.registerAt = LocalDate.now();
+    user.role = Role.USER;
+    return user;
+  }
 }
