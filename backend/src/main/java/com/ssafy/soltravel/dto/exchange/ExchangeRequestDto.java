9676491fe2ad31c2f09530871fa667688da44d91
이진주 <dlwlswn3539@naver.com>
2024-08-21 23:23:06 +0900

    [BE] Feat: 환전 실행 api 구현

---
+++ backend/src/main/java/com/ssafy/soltravel/dto/exchange/ExchangeRequestDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.soltravel.dto.exchange;
+
+public class ExchangeRequestDto {
+
+  private String accountNo;
+  private String exchangeCurrency;
+  private Double exchangeAmount;
+}
