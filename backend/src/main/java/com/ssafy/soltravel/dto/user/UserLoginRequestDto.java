135b15e001dc07791fc8c2d67d3f0b0fe93f91af
허동원 <heo_dongwon@naver.com>
2024-08-22 22:02:27 +0900

    [BE] Feat: 회원가입/ 로그인 요청 Dto 검증 구현

--- backend/src/main/java/com/ssafy/soltravel/dto/user/UserLoginRequestDto.java
+++ backend/src/main/java/com/ssafy/soltravel/dto/user/UserLoginRequestDto.java
@@ -1,6 +1,8 @@
 package com.ssafy.soltravel.dto.user;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserLoginRequestDto {
 
+  @NotNull
+  @NotBlank
   private String email;
+
+  @NotNull
+  @NotBlank
   private String password;
 }
