8b89b1b5f3a785fedd008a4b7a9dd86ce2495a7d
이성현 <vkse1934@naver.com>
2024-08-24 20:09:40 +0900

    [BE] Feat: Participant 관련 Dto 생성

---
+++ backend/src/main/java/com/ssafy/soltravel/dto/participants/ParticipantDto.java
@@ -0,0 +1,26 @@
+package com.ssafy.soltravel.dto.participants;
+
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantDto {
+
+    private Long participantId;
+
+    private Long userId;
+
+    private boolean isMaster;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
