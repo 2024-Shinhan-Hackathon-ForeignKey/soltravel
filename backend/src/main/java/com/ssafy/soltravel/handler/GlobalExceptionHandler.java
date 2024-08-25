d3f775955c43f35ae5c12639b917ad1feb673a18
이성현 <vkse1934@naver.com>
2024-08-25 13:07:19 +0900

    [BE] Feat: 계좌 해지 구현

--- backend/src/main/java/com/ssafy/soltravel/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/soltravel/handler/GlobalExceptionHandler.java
@@ -13,47 +13,48 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ResponseDto> handleGeneralException(Exception e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDto> handleGeneralException(Exception e) {
 
-    ResponseDto errorResponseDto = new ResponseDto();
-    errorResponseDto.setStatus("INTERNAL_SERVER_ERROR");
-    errorResponseDto.setMessage(e.getMessage());
+        ResponseDto errorResponseDto = new ResponseDto();
+        errorResponseDto.setStatus("INTERNAL_SERVER_ERROR");
+        errorResponseDto.setMessage(e.getMessage());
 
-    return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-  @ExceptionHandler(WebClientResponseException.class)
-  public ResponseEntity<String> handleWebClientResponseException(WebClientResponseException e) {
-    return new ResponseEntity<String>(e.getResponseBodyAsString(), HttpStatus.BAD_REQUEST);
-  }
+    @ExceptionHandler(WebClientResponseException.class)
+    public ResponseEntity<String> handleWebClientResponseException(WebClientResponseException e) {
+        e.printStackTrace();
+        return new ResponseEntity<String>(e.getResponseBodyAsString(), HttpStatus.BAD_REQUEST);
+    }
 
-  @ExceptionHandler(UserNotFoundException.class)
-  public ResponseEntity<ResponseDto> handleUserNotFoundException(UserNotFoundException e) {
-    ResponseDto errorResponse = new ResponseDto(
-        "NOT_FOUND",
-        String.format("DB에 해당 유저가 없습니다: %d", e.getUserId())
-    );
-    return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-  }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ResponseDto> handleUserNotFoundException(UserNotFoundException e) {
+        ResponseDto errorResponse = new ResponseDto(
+            "NOT_FOUND",
+            String.format("DB에 해당 유저가 없습니다: %d", e.getUserId())
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
-  @ExceptionHandler(InvalidCredentialsException.class)
-  public ResponseEntity<ResponseDto> handleInvalidCredentialsException(
-      InvalidCredentialsException e) {
-    ResponseDto errorResponse = new ResponseDto(
-        "UNAUTHORIZED",
-        e.getMessage()
-    );
-    return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-  }
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ResponseDto> handleInvalidCredentialsException(
+        InvalidCredentialsException e) {
+        ResponseDto errorResponse = new ResponseDto(
+            "UNAUTHORIZED",
+            e.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
 
-  @ExceptionHandler(InvalidAuthCodeException.class)
-  public ResponseEntity<ResponseDto> handleInvalidAuthCodeException(
-      InvalidAuthCodeException e) {
-    ResponseDto errorResponse = new ResponseDto(
-        "UNAUTHORIZED",
-        e.getMessage()
-    );
-    return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-  }
+    @ExceptionHandler(InvalidAuthCodeException.class)
+    public ResponseEntity<ResponseDto> handleInvalidAuthCodeException(
+        InvalidAuthCodeException e) {
+        ResponseDto errorResponse = new ResponseDto(
+            "UNAUTHORIZED",
+            e.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
 }
