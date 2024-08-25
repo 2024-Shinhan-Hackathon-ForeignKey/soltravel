379b8fbc2c6452d7ef640c86bf984c5727bdd13c
이진주 <dlwlswn3539@naver.com>
2024-08-25 23:37:41 +0900

    [BE] Feat: NotifiacationController SecurityUtil적용 및 Swagger 적용

--- backend/src/main/java/com/ssafy/soltravel/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/soltravel/controller/NotificationController.java
@@ -1,6 +1,12 @@
 package com.ssafy.soltravel.controller;
 
 import com.ssafy.soltravel.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +17,21 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 @RequestMapping("/notification")
 @RequiredArgsConstructor
+@Tag(name = "Notification API", description = "알림 관련 API")
 public class NotificationController {
 
   private final NotificationService notificationService;
 
-  // 메시지 알림 구독
-  @GetMapping("/subscribe/{userId}")
-  public SseEmitter subscribe(@PathVariable Long userId) {
+  /**
+   * 메시지 알림 구독
+   */
+  @GetMapping("/subscribe")
+  @Operation(summary = "메시지 알림 구독", description = "특정 사용자의 메시지 알림을 구독합니다. SSE를 통해 실시간 알림을 수신합니다.", responses = {
+      @ApiResponse(responseCode = "200", description = "성공적으로 알림을 구독했습니다.", content = @Content(schema = @Schema(implementation = SseEmitter.class))),
+      @ApiResponse(responseCode = "404", description = "해당 사용자를 찾을 수 없습니다.", content = @Content),
+      @ApiResponse(responseCode = "500", description = "서버 오류입니다.", content = @Content)})
+  public SseEmitter subscribe() {
 
-    SseEmitter sseEmitter = notificationService.subscribe(userId);
-    return sseEmitter;
+    return notificationService.subscribe();
   }
 }
