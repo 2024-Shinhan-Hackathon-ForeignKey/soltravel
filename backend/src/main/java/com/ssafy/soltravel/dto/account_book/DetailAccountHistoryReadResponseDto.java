519ad5fc7d8e8a17dd0efc24bfeeaa269112ed02
허동원 <heo_dongwon@naver.com>
2024-08-29 22:08:20 +0900

    [BE] Feat: 가계부 상세 조회 기능 구현  - 요청, 응답 Dto 작성  - Mapper 메서드 구현  (DetailAccountHistoryReadRequestDto -> TransactionHistoryRequestDto)
     - AccountHistoryReadRequestDto 스웨거 수정

---
+++ backend/src/main/java/com/ssafy/soltravel/dto/account_book/DetailAccountHistoryReadResponseDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.soltravel.dto.account_book;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DetailAccountHistoryReadResponseDto {
+
+  private String amount;
+  private String transactionType;
+  private LocalDateTime transactionAt;
+  private String balance;
+  private String store;
+}
--- backend/src/main/java/com/ssafy/soltravel/mapper/TransactionMapper.java
+++ backend/src/main/java/com/ssafy/soltravel/mapper/TransactionMapper.java
@@ -1,11 +1,15 @@
 package com.ssafy.soltravel.mapper;
 
+import com.ssafy.soltravel.domain.Enum.OrderByType;
 import com.ssafy.soltravel.dto.account_book.AccountHistoryReadRequestDto;
+import com.ssafy.soltravel.dto.account_book.DetailAccountHistoryReadRequestDto;
 import com.ssafy.soltravel.dto.transaction.request.TransactionHistoryRequestDto;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TransactionMapper {
 
-  public static TransactionHistoryRequestDto convertaccountToTransaction(
+  public static TransactionHistoryRequestDto convertAccountToTransaction(
       AccountHistoryReadRequestDto accountDto
   ) {
     return TransactionHistoryRequestDto.builder()
@@ -13,6 +17,28 @@ public class TransactionMapper {
         .endDate(accountDto.getEndDate())
         .transactionType(accountDto.getTransactionType())
         .orderByType(accountDto.getOrderByType())
+        .build();
+  }
+
+
+  public static TransactionHistoryRequestDto convertDetailAccountToTransaction(
+      DetailAccountHistoryReadRequestDto detailDto
+  ) {
+    // "yyyymmdd" 형식의 문자열을 LocalDate로 변환
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    LocalDate date = LocalDate.parse(detailDto.getDate(), formatter);
+
+    // 다음 날로 변경
+    LocalDate nextDay = date.plusDays(1);
+
+    // 다시 "yyyymmdd" 형식의 문자열로 변환
+    String endDate = nextDay.format(formatter);
+
+    return TransactionHistoryRequestDto.builder()
+        .startDate(detailDto.getDate())
+        .endDate(endDate)
+        .transactionType(detailDto.getTransactionType())
+        .orderByType(OrderByType.ASC)
         .build();
   }
 
