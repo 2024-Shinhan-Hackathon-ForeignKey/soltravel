8988613dd467801dc5e9dc1d20b86ace5d14c9f8
허동원 <heo_dongwon@naver.com>
2024-08-31 06:07:03 +0900

    [BE] Fix: Swagger URL 변경

--- backend/src/main/java/com/ssafy/soltravel/BackendApplication.java
+++ backend/src/main/java/com/ssafy/soltravel/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.ssafy.soltravel;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://soltravel.shop/api/v1", description = "default server url")})
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
