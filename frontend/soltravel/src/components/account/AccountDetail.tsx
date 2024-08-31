b7e4b3d5148f948f958be09c981690b2e00b653f
허동원 <heo_dongwon@naver.com>
2024-08-31 12:37:34 +0900

    [FE] Feat: 희망 환율 조회 기능 추가

--- frontend/soltravel/src/components/account/AccountDetail.tsx
+++ frontend/soltravel/src/components/account/AccountDetail.tsx
@@ -31,10 +31,14 @@ const AccountDetail = ({ isLeader, account, foreignAccount }: Props) => {
               <div className="rounded-md flex justify-between">
                 <div>
                   <p className="text-sm font-bold">올인원 일반모임통장</p>
-                  <p className="text-sm text-zinc-500">{formatAccountNumber(account.accountNo)}</p>
+                  <p className="text-sm text-zinc-500">
+                    {formatAccountNumber(account.accountNo)}
+                  </p>
                 </div>
                 <div className="flex items-center">
-                  <p className="text-[1.3rem] font-semibold">{formatCurrency(account.balance)}</p>
+                  <p className="text-[1.3rem] font-semibold">
+                    {formatCurrency(account.balance)}
+                  </p>
                   <p className="text-[1rem]">원</p>
                 </div>
               </div>
@@ -44,7 +48,8 @@ const AccountDetail = ({ isLeader, account, foreignAccount }: Props) => {
                     e.stopPropagation();
                     navigate("/transaction");
                   }}
-                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold">
+                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold"
+                >
                   입금
                 </button>
                 <button
@@ -52,7 +57,8 @@ const AccountDetail = ({ isLeader, account, foreignAccount }: Props) => {
                     e.stopPropagation();
                     navigate(`/accounthistory/${account.accountNo}`);
                   }}
-                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold">
+                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold"
+                >
                   내역
                 </button>
               </div>
@@ -63,37 +69,53 @@ const AccountDetail = ({ isLeader, account, foreignAccount }: Props) => {
 
       {foreignAccount && (
         <div>
-          <p className="text-sm mb-3 font-bold">외화모임통장</p>
+          <div className="flex justify-between">
+            <p className="text-sm mb-3 font-bold">외화모임통장</p>
+            <p>
+              {" "}
+              희망 환율: {account?.preferenceExchange}{" "}
+              {foreignAccount.currency.currencyCode}{" "}
+            </p>
+          </div>
           <div className="w-full py-5 px-5 flex flex-col rounded-xl bg-white shadow-md">
             <div className="rounded-md flex justify-between mb-3">
               <div className="flex flex-col">
                 <p className="text-sm font-bold">올인원 외화모임통장</p>
-                <p className="text-sm text-zinc-500">{formatAccountNumber(foreignAccount.accountNo)}</p>
+                <p className="text-sm text-zinc-500">
+                  {formatAccountNumber(foreignAccount.accountNo)}
+                </p>
               </div>
               <div className="flex items-center space-x-[0.1rem]">
-                <p className="text-[1.3rem] font-semibold">{formatCurrency(foreignAccount.balance)}</p>
-                <p className="text-[1rem]">{foreignAccount.currency.currencyCode}</p>
+                <p className="text-[1.3rem] font-semibold">
+                  {formatCurrency(foreignAccount.balance)}
+                </p>
+                <p className="text-[1rem]">
+                  {foreignAccount.currency.currencyCode}
+                </p>
               </div>
             </div>
             <div className="flex space-x-3">
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    navigate("/exchange");
-                  }}
-                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold">
-                  환전
-                </button>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    navigate(`/accounthistory/${foreignAccount.accountNo}`);
-                  }}
-                  className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold">
-                  내역
-                </button>
-              </div>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  navigate("/exchange");
+                }}
+                className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold"
+              >
+                환전
+              </button>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  navigate(`/accounthistory/${foreignAccount.accountNo}`);
+                }}
+                className="w-full h-9 p-2 rounded-md bg-[#0046FF] text-white text-sm font-bold"
+              >
+                내역
+              </button>
+            </div>
           </div>
+
           <div className="fixed bottom-2 left-1 right-1 p-4">
             <button className="w-full h-12 rounded-md bg-[#0046FF] text-white text-sm font-bold">
               정산하기
